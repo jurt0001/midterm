6282739641db10ad92d32fa1b1904d5b077e5076
christianjurt <Longbeach@2>
2017-10-19 16:31:28 -0400

    updated app to display issue with prop validation

--- contactsApp/src/index.js
+++ contactsApp/src/index.js
@@ -1,15 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-
-//var a = () => console.log("Hello World");
-//debugger;
-
-//var b;
-
-//a();
-
-
 //*************************************************************
 // Author: Christian Jurt
 // File: Main.js - This is the main (JSX) page.
@@ -20,128 +11,28 @@ import ReactDOM from "react-dom";
 
 'use strict';
 
-//const App = (props) => {
-//
-//    return(
-//        <h1>
-//            {props.name} ({props.age})
-//        </h1>
-//       
-//    );
-//};
-//
-//
-//ReactDOM.render(
-//    <App name="Christian" age="35"/>, 
-//    document.getElementById("react-app")
-//);
+const App = (props) => {
 
-
-
-class ContactsApp extends React.Component {
-     constructor() {
-    super();
-      
-  }
-    
-  render() {
-    return (
-      <div className="container">
-        
-      </div>
-    );
-  } 
-}
-
-
-class ContactBox extends React.Component {
-
-    render(){
+    return(
+        <h1>
+            {props.name} ({props.age})
+        </h1>
        
     );
-        
+};
 
-        //const myContacts = this._getContacts();
-        return ( 
-            <div className="contact-box">
-            
-            <h3>Contacts</h3>
-            
-            <div className="contact-list">
-            <ul>
-            {contactItem}
-            </ul>
-            
-            </div>
-            </div>
-           
-        )
-    }
-                                           
-    )}
+App.propTypes = {
+    
+    name: React.propTypes.string,
+    age: React.propTypes.number
 }
 
+ReactDOM.render(
+    <App name="Christian" age="35"/>, 
+    document.getElementById("react-app")
+);
 
-class ContactItem extends React.Component{
-  render() {
-    return(
-      <li>
-        <strong>{this.props.name}</strong> - {this.props.email}
-      </li>
-    );
-  }
-} 
 
-  let contacts = [{
-    "gender": "male",
-    "name": {"title": "mr", "first": "daniel", "last": "williams"},
-    "location": {"street": "6366 duke st", "city": "kingston", "state": "northwest territories", "postcode": 90447},
-    "email": "daniel.williams@example.com",
-    "dob": "1957-03-13 04:24:26",
-    "phone": "955-142-0585",
-    "cell": "689-368-5264",
-    "id": 1001,
-    "picture": {
-        "large": "https://randomuser.me/api/portraits/men/82.jpg",
-        "medium": "https://randomuser.me/api/portraits/med/men/82.jpg",
-        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/82.jpg"
-    },
-    "key": 1
-}, {
-    "gender": "male",
-    "name": {"title": "mr", "first": "mason", "last": "lavoie"},
-    "location": {"street": "7838 queen st", "city": "sandy lake", "state": "northwest territories", "postcode": 11887},
-    "email": "mason.lavoie@example.com",
-    "dob": "1979-09-14 07:40:46",
-    "phone": "498-109-2176",
-    "cell": "809-132-5817",
-    "id": 1002,
-    "picture": {
-        "large": "https://randomuser.me/api/portraits/men/37.jpg",
-        "medium": "https://randomuser.me/api/portraits/med/men/37.jpg",
-        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/37.jpg"
-    },
-    "key": 2
-}];
 
-ReactDOM.render(<ContactBox />, document.getElementById("react-app"));
-
-//class App extends React.Component {
-//    constructor(){
-//        super();
-//        
-//    }
-//    
-//   render(props){ 
-//   return(
-//        <h1>
-//            {props.headerMessage}
-//        
-//        </h1>
-//    );  
-//    
-//    
-//   }
-//}
 
 
